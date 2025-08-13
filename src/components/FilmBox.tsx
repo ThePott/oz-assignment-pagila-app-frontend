5f4e8044975ecb348678e74d4f7ca42f1d8cbf1c
Tynahan <nusilite@gmail.com>
2025-08-13 16:15:45 +0900

    ✨ Feat(generic comp): Make RoundedBox generic

--- src/components/FilmBox.tsx
+++ src/components/FilmBox.tsx
@@ -2,8 +2,11 @@ import type { Film } from "../interfaces"
 import RoundedBox from "./RoundedBox"
 
 const FilmBox = ({ film }: { film: Film }) => {
+  const handleClick = () => {
+    console.log({clicked: film.film_id})
+  }
   return (
-    <RoundedBox>
+    <RoundedBox onClick={handleClick}>
       <p className="text-2xl font-semibold">{`${film.title} (${film.release_year})`}</p>
       <p>{film.description}</p>
     </RoundedBox>
--- src/components/RoundedBox.tsx
+++ src/components/RoundedBox.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const RoundedBox = ({ children }: { children: React.ReactNode }) => {
+const RoundedBox = (props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>) => {
+    const {children, ...rest} = props
     return (
-        <div className="m-3 p-3 rounded-xl border-1 border-amber-600">{children}</div>
+        <div {...rest} className="m-3 p-3 rounded-xl border-1 border-amber-600">{children}</div>
     )
 }
 
